3a4106d8bea22eacf26082e3768063b66460b38e
DevMoath0 <devmoath0@gmail.com>
2025-08-04 18:13:08 +0300

    Base Files

---
+++ src/main/java/sa/com/moath/cachingservice/CachingServiceApplication.java
@@ -0,0 +1,13 @@
+package sa.com.moath.cachingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingServiceApplication.class, args);
+    }
+
+}
